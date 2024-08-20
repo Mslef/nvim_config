98f9c2128adc450b238181c14fec625a900b156d
Marcel Lefebvre <marcel.lefebvre01@gmail.com>
2024-08-20 15:10:29 -0400

    Telescope update for zotero, text editor & keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,6 +1,3 @@
-
-vim.keymap.set("n", "<leader>tk", ":Telescope keymaps<CR>", {})
-
 vim.keymap.set("n", "<C-d>", "<C-d>zz", {})
 vim.keymap.set("n", "<C-u>", "<C-u>zz", {})
 vim.keymap.set("n", "n", "nzz", {})
@@ -8,35 +5,25 @@ vim.keymap.set("n", "N", "Nzz", {})
 vim.keymap.set("n", "N", "Nzz", {})
 vim.keymap.set("n", "k", "gk", {})
 vim.keymap.set("n", "j", "gj", {})
-vim.keymap.set({ "i", "v" }, "<c-space>", "<esc>", {})
-vim.keymap.set("n", "<leader>wd", ":w<CR>", {})
-vim.keymap.set("n", "<leader>qq", ":bp|bd #<CR>", {})
-vim.keymap.set("n", "<leader>ex", ":Oil<cr>", {})
-
-vim.keymap.set("n", "<leader>spl", ":vsplit<CR>", {})
-vim.keymap.set("n", "<leader>cs", ":nohlsearch<CR>", {})
 vim.keymap.set("n", "<C-h>", "<C-w>h", {})
 vim.keymap.set("n", "<C-l>", "<C-w>l", {})
 vim.keymap.set("n", "<C-k>", "<C-w>k", {})
 vim.keymap.set("n", "<C-j>", "<C-w>j", {})
 
-vim.keymap.set("n", "<leader>tf", ":Telescope find_files<CR>", {})
-vim.keymap.set("n", "<leader>ts", ":Telescope live_grep<CR>", {})
-vim.keymap.set("n", "<leader>tn", ":Telescope notify<CR>", {})
-
--- Diagnostics
-vim.keymap.set("n", "<leader>td", ":Telescope diagnostics<CR>", {})
-vim.keymap.set("n", "<leader>do", vim.diagnostic.open_float, {})
-vim.keymap.set("n", "<leader>dh", vim.diagnostic.goto_prev, {})
-vim.keymap.set("n", "<leader>dl", vim.diagnostic.goto_next, {})
-vim.keymap.set("n", "<leader>dac", vim.lsp.buf.code_action, {})
-
--- Obsidian
-vim.keymap.set("n", "<leader>on", ":ObsidianNew<CR>", {})
-vim.keymap.set("n", "<leader>to", ":ObsidianSearch<CR>", {})
-vim.keymap.set("n", "<leader>z", ":ZenMode<CR>", {})
+vim.keymap.set("n", "<leader>co", ":e ~/.config/nvim/.<cr>", {})
+vim.keymap.set("n", "<leader>t", ":term<cr>i", {})
+vim.keymap.set("t", "<Esc>", "<c-\\><c-n>", {})
+vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, {})
+vim.keymap.set("n", "<leader>j", vim.diagnostic.goto_prev, {})
+vim.keymap.set("n", "<leader>k", vim.diagnostic.goto_next, {})
+vim.keymap.set("n", "<leader>ac", vim.lsp.buf.code_action, {})
 
 vim.keymap.set("n", "<leader>db", ":DBUIToggle<CR>", {})
+vim.keymap.set("n", "<leader>x", ":Oil<cr>", {})
+
+-- À éliminer
+vim.keymap.set("n", "<leader>cs", ":noh<CR>", {})
+vim.keymap.set("n", "<leader>qq", ":bp|bd #<CR>", {})
 
 vim.api.nvim_create_autocmd("LspAttach", {
 	desc = "LSP actions",
@@ -49,28 +36,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 	end,
 })
-
-local builtin = require("telescope.builtin")
-local action_state = require("telescope.actions.state")
-
-vim.keymap.set("n", "<leader>bb", function()
-	builtin.buffers({
-		initial_mode = "normal",
-		attach_mappings = function(prompt_bufnr, map)
-			local delete_buf = function()
-				local current_picker = action_state.get_current_picker(prompt_bufnr)
-				current_picker:delete_selection(function(selection)
-					vim.api.nvim_buf_delete(selection.bufnr, { force = true })
-				end)
-			end
-
-			map("n", "<c-d>", delete_buf)
-
-			return true
-		end,
-	}, {
-		sort_lastused = true,
-		sort_mru = true,
-		theme = "dropdown",
-	})
-end)
--- lua/plugins/textEditor.lua
+++ lua/plugins/textEditor.lua
@@ -1,51 +1,26 @@
+Notes_subdir = "/Users/marcellefebvre/Library/Mobile Documents/iCloud~md~obsidian/Documents/Notes/"
+
+CreateNote = function()
+	vim.cmd("e " .. Notes_subdir .. os.date("%d%m%Y%H%M") .. ".md")
+end
+
+SearchNotes = function()
+	vim.cmd("lcd " .. Notes_subdir .. " | Telescope live_grep")
+end
+
+vim.api.nvim_create_user_command("CreateNote", CreateNote, {})
+vim.api.nvim_create_user_command("SearchNotes", SearchNotes, {})
+vim.keymap.set("n", "<leader>n", ":CreateNote<cr>", {})
+vim.keymap.set("n", "<leader>sn", ":SearchNotes<cr>", {})
+
 vim.api.nvim_create_autocmd("BufEnter", {
 	pattern = "*.md",
 	callback = function(args)
-		require("zen-mode").reset({})
-		-- vim.api.nvim_open_win(0, true, { relative = "win", row = 3, col = 3, width = 12, height = 3 })
-		vim.cmd("set norelativenumber nonumber")
-		vim.cmd("LspStop")
+		require("cmp").setup.buffer({ sources = {} })
+		vim.cmd("setlocal norelativenumber nonumber")
+		vim.cmd("setlocal foldcolumn=9")
+		vim.cmd("setlocal columns=150")
 	end,
 })
 
-return {
-	{
-		"folke/zen-mode.nvim",
-		dependencies = { "folke/twilight.nvim" },
-		opts = {
-			window = { width = 0.8 },
-			plugins = { tmux = { enabled = true } },
-		},
-	},
-	{
-		"epwalsh/obsidian.nvim",
-		version = "*",
-		lazy = false,
-		ft = "markdown",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-		config = function()
-			require("obsidian").setup({
-				workspaces = {
-					{
-						name = "Notes",
-						path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents",
-					},
-				},
-				completion = {
-					nvim_cmp = true,
-					min_char = 2,
-				},
-				notes_subdir = "notes",
-				note_id_func = function(title)
-					if title ~= nil then
-						return title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-					else
-						return os.date("%d%m%Y%H%M")
-					end
-				end,
-			})
-		end,
-	},
-}
+return {}
