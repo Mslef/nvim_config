491a11ed5e3e79a0f38ba7d68e37cfbf04042f87
Marcel Lefebvre <marcel.lefebvre01@gmail.com>
2025-05-01 09:15:32 -0400

    update terminal config

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,27 +1,26 @@
 vim.keymap.set("n", "<C-d>", "<C-d>zz", {})
 vim.keymap.set("n", "<C-u>", "<C-u>zz", {})
-vim.keymap.set("n", "n", "nzz", {})
-vim.keymap.set("n", "N", "Nzz", {})
-vim.keymap.set("n", "k", "gk", {})
-vim.keymap.set("n", "j", "gj", {})
 vim.keymap.set("n", "<C-h>", "<C-w>h", {})
 vim.keymap.set("n", "<C-l>", "<C-w>l", {})
 vim.keymap.set("n", "<C-k>", "<C-w>k", {})
 vim.keymap.set("n", "<C-j>", "<C-w>j", {})
 
 vim.keymap.set("n", "<leader>t", "<cmd>term<cr>i", {})
-vim.keymap.set("t", "<c-x>", "<c-\\><c-n>", {})
+
+vim.keymap.set("t", "<esc>", "<c-\\><c-n>", {})
+vim.keymap.set("t", "<C-h>", "<c-\\><c-n><C-w>h", {})
+vim.keymap.set("t", "<C-l>", "<c-\\><c-n><C-w>l", {})
 
 vim.keymap.set("n", "<leader>gg", "<cmd>te lazygit<cr><cmd>setlocal number! relativenumber!<cr>i", {})
 vim.keymap.set("n", "<leader>vd", "<cmd>te vd<cr><cmd>setlocal number! relativenumber!<cr>i", {})
 vim.keymap.set("n", "<leader>db", "<cmd>DBUIToggle<CR>", {})
 vim.keymap.set("n", "<leader>x", "<cmd>Oil<cr>", {})
+
 vim.keymap.set("n", "<leader>f", "<cmd>Telescope find_files<CR>", {})
 vim.keymap.set("n", "<leader>s", "<cmd>Telescope live_grep<CR>", {})
-vim.keymap.set("n", "<leader>m", "<cmd>Telescope notify<CR>", {})
-vim.keymap.set("n", "<leader>dd", "<cmd>Telescope diagnostics<CR>", {})
-vim.keymap.set("n", "<leader>ct", "<cmd>lua require('telescope.builtin').colorscheme({enable_preview = true})<CR>", {})
+vim.keymap.set("n", "<leader>d", "<cmd>Telescope diagnostics<CR>", {})
 
+vim.keymap.set("i", "<c-b>", "`", {})
 
 vim.keymap.set("i", "<left>", '<cmd>echo "no left! Only l!"<cr>', {})
 vim.keymap.set("i", "<right>", '<cmd>echo "no right! Only h!"<cr>', {})
@@ -31,13 +30,13 @@ vim.keymap.set("i", "<pageup>", '<cmd>echo "no page up!"<cr>', {})
 vim.keymap.set("i", "<pagedown>", '<cmd>echo "no page down!"<cr>', {})
 
 vim.api.nvim_create_autocmd("LspAttach", {
-  desc = "LSP actions",
-  callback = function(event)
-    local opts = { buffer = event.buf }
-    vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
-    vim.keymap.set("n", "<leader>gt", vim.lsp.buf.type_definition, opts)
-    vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, {})
-    vim.keymap.set("n", "<leader>j", vim.diagnostic.goto_prev, {})
-    vim.keymap.set("n", "<leader>k", vim.diagnostic.goto_next, {})
-  end,
+	desc = "LSP actions",
+	callback = function(event)
+		local opts = { buffer = event.buf }
+		vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "<leader>gt", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "<leader>of", vim.diagnostic.open_float, {})
+		vim.keymap.set("n", "<leader>j", vim.diagnostic.goto_prev, {})
+		vim.keymap.set("n", "<leader>k", vim.diagnostic.goto_next, {})
+	end,
 })
--- lua/plugins/lspConfig.lua
+++ lua/plugins/lspConfig.lua
@@ -1,46 +1,68 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig", "hrsh7th/cmp-nvim-lsp" },
-    lazy = false,
-    config = function()
-      require("mason").setup()
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = require("cmp_nvim_lsp").default_capabilities(),
-          })
-          if server_name == "lua_ls" then
-            require("lspconfig")[server_name].setup({
-              settings = {
-                Lua = {
-                  runtime = { version = "LuaJIT" },
-                  diagnostics = { globals = { "vim" } },
-                  workspace = {
-                    library = { vim.env.VIMRUNTIME },
-                  },
-                },
-              },
-            })
-          end
-        end,
-      })
-    end,
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    lazy = false,
-    config = function()
-      require("nvim-treesitter.configs").setup({
-        highlight = { enable = true },
-        indent = { enable = true },
-        ensure_installed = {
-          "bash", "astro", "c", "css", "html", "luadoc", "luap", "query", "regex", "tsx", "typescript", "vimdoc", "bash",
-          "javascript", "json", "lua", "markdown", "markdown_inline", "python", "vim", "yaml", "julia", "r",
-        },
-      })
-    end,
-  },
+	{
+		"williamboman/mason-lspconfig.nvim",
+		dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig", "hrsh7th/cmp-nvim-lsp" },
+		lazy = false,
+		config = function()
+			require("mason").setup()
+			require("mason-lspconfig").setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = require("cmp_nvim_lsp").default_capabilities(),
+					})
+					if server_name == "lua_ls" then
+						require("lspconfig")[server_name].setup({
+							settings = {
+								Lua = {
+									runtime = { version = "LuaJIT" },
+									diagnostics = { globals = { "vim" } },
+									workspace = {
+										library = { vim.env.VIMRUNTIME },
+									},
+								},
+							},
+						})
+					end
+				end,
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		lazy = false,
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				highlight = { enable = true },
+				indent = { enable = true },
+				ensure_installed = {
+					"bash",
+					"astro",
+					"c",
+					"css",
+					"html",
+					"luadoc",
+					"luap",
+					"query",
+					"regex",
+					"tsx",
+					"typescript",
+					"vimdoc",
+					"bash",
+					"javascript",
+					"json",
+					"lua",
+					"markdown",
+					"markdown_inline",
+					"python",
+					"vim",
+					"yaml",
+					"julia",
+					"r",
+					"templ",
+					"zig",
+				},
+			})
+		end,
+	},
 }
-
